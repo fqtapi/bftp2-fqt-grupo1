82d90245b34b3548c82aecb3f83e687db9d85ddb
desimorenoh <desimorenoh@gmail.com>
2022-03-09 18:18:00 +0100

    controller

--- backend/src/main/java/com/example/bftp2springprofilesexample/StockController.java
+++ backend/src/main/java/com/example/bftp2springprofilesexample/StockController.java
@@ -2,9 +2,11 @@ package com.example.bftp2springprofilesexample;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StockController {
@@ -20,8 +22,33 @@ public class StockController {
         return stockRepository.findAll();
     }
 
-    @GetMapping("/prueba")
-    public List<Stock> getPrueba() {
-        return stockRepository.findAll();
+    @GetMapping("/category/prendas")
+    public List<Stock> prendas(@RequestParam Map<String, String> category) {
+
+        return stockRepository.findAllByCategoryContains("prendas");
     }
+
+    @GetMapping("/category/complementos")
+    public List<Stock> complementos(@RequestParam Map<String, String> parameters) {
+        if (parameters.containsKey("complementos")) {
+        }
+        return stockRepository.findAllByCategoryContains("complementos");
+    }
+
+    @GetMapping("/category/accesorios")
+    public String accesorios(@RequestParam Map<String, String> codigo) {
+        if (codigo.containsKey("accesorios")) {
+        }
+        return String.valueOf(stockRepository.findAllByCategoryContains("accesorios"));
+    }
+
+    @GetMapping("/category/cosmeticos")
+    public String cosmeticos(@RequestParam Map<String, String> parameters) {
+        if (parameters.containsKey("cosmeticos")) {
+        }
+        return "No existe";
+    }
+
 }
+
+
--- backend/src/main/java/com/example/bftp2springprofilesexample/StockRepository.java
+++ backend/src/main/java/com/example/bftp2springprofilesexample/StockRepository.java
@@ -2,5 +2,8 @@ package com.example.bftp2springprofilesexample;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock,Long> {
+      List<Stock> findAllByCategoryContains(String category);
 }
