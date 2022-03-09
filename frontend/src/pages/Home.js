820ca1bc89c23ba836d28bea0c5e9c5c8b787d2f
Laura Contreras <m.laura.cp89@gmail.com>
2022-03-09 16:06:35 +0100

    tarjetas

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "./Home.css"
+import prendas from "../assets/img-cards/prendas.jpg"
+import complementos from "../assets/img-cards/complementos.jpg"
+import accesorios from "../assets/img-cards/accesorios.jpg"
+import cosmeticos from "../assets/img-cards/cosmeticos.jpg"
+
 
 function Home(){
     return(
         <div className="home">
             <Navbar />
-            <h1>Aquí va el home</h1>
+            <div className="card">
+                <img src={prendas} className="image-card"/>
+                <button>Prendas</button>
+            </div>
+            <div className="card">
+                <img src={complementos} className="image-card"/>
+                <button>Complementos</button>
+            </div>
+            <div className="card">
+                <img src={accesorios} className="image-card"/>
+                <button>Accesorios</button>
+            </div>
+            <div className="card">
+                <img src={cosmeticos} className="image-card"/>
+                <button>Cosméticos</button>
+            </div>
             <Footer/>
         </div>
     );
