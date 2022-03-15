8be8ef89491f366be3accdeb52042b0dc5c001e8
desimorenoh <desimorenoh@gmail.com>
2022-03-15 12:23:22 +0100

    pag login lista

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -13,12 +13,14 @@ function Login(props) {
         <div className="login">
             <section className="login-section">
                 <img className="logo-fqt-log" src={logo} alt={"Quiero Trabajo logo"}/>
+                <h2 className="login-title">FUNDACIÓN QUIERO TRABAJO</h2>
+
                 <form className="login-form" onSubmit={handleSubmit}>
-                    <span><label className="label-login" htmlFor="userInput">User:  </label>
+                    <span><label className="label-login" htmlFor="userInput">Usuario:  </label>
                     <input className="input-login" type="text"/></span>
-                    <span><label className="label-login" htmlFor="passwordInput">Password:  </label>
+                    <span><label className="label-login" htmlFor="passwordInput">Contraseña:  </label>
                     <input className="input-login" type="password"/></span>
-                    <input className="css-button-rounded--red" type="submit"/>
+                    <input className="css-button-rounded--red" type="submit" value="ACCEDER"/>
                 </form>
             </section>
         </div>
