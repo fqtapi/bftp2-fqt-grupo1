338347b9f3894c596537dabfb50ae75ea6edd129
desimorenoh <desimorenoh@gmail.com>
2022-03-16 11:07:16 +0100

    imagenes en card done

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import "./Card.css"
-import prendas from '../../assets/img-cards/prendas.jpg';
-
+import prendasimg from '../../assets/img-cards/prendas.jpg';
+import accesoriosimg from "../../assets/img-cards/accesorios.jpg";
+import complementosimg from "../../assets/img-cards/complementos.jpg";
+import cosmeticosimg from "../../assets/img-cards/cosmeticos.jpg";
 
 function Card({category, onClick}) {
-    return (   <div className="stock-form">
-        <div className="card" onClick={onClick}>
-            <img src={prendas} className="img-card" alt="prendas"/>
-            <button className="css-button-rounded--red" onClick={onClick}>{category}</button>
 
-        </div>
 
-</div>
-    );
+    if (category === "Prendas") {
+        return (
+            <div className="stock-form">
+                <div className="card" onClick={onClick}>
+                    <img src={prendasimg} className="img-card" alt="prendas"/>
+                    <button className="css-button-rounded--red" onClick={onClick}>{category}</button>
+                </div>
+            </div>
+        )
+    } else if (category === "Accesorios") {
+        return (
+            <div className="stock-form">
+                <div className="card" onClick={onClick}>
+                    <img src={accesoriosimg} className="img-card" alt="accesorios"/>
+                    <button className="css-button-rounded--red" onClick={onClick}>{category}</button>
+                </div>
+            </div>
+        )
+    } else if (category === "Complementos") {
+        return (
+            <div className="stock-form">
+                <div className="card" onClick={onClick}>
+                    <img src={complementosimg} className="img-card" alt="complementos"/>
+                    <button className="css-button-rounded--red" onClick={onClick}>{category}</button>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="stock-form">
+                <div className="card" onClick={onClick}>
+                    <img src={cosmeticosimg} className="img-card" alt="cosmeticos"/>
+                    <button className="css-button-rounded--red" onClick={onClick}>{category}</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Card;
