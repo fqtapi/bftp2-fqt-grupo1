3eedb69c803617ee9ce827e3c4efefcea8ecb7d7
desimorenoh <desimorenoh@gmail.com>
2022-03-17 15:42:47 +0100

    fix problem in stock table?

--- backend/src/main/java/com/example/bftp2springprofilesexample/StockRepository.java
+++ backend/src/main/java/com/example/bftp2springprofilesexample/StockRepository.java
@@ -1,9 +1,10 @@
 package com.example.bftp2springprofilesexample;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional
 public interface StockRepository extends JpaRepository<Stock,Long> {
       List<Stock> findAllByCategoryContains(String category);
 }
