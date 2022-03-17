d65c95f7cc03dac930b3507d739d6e077a8272dd
desimorenoh <desimorenoh@gmail.com>
2022-03-17 14:15:16 +0100

    map test

--- frontend/src/table/StockTable.js
+++ frontend/src/table/StockTable.js
@@ -3,7 +3,9 @@ import "./Table.css";
 import {FaEdit} from 'react-icons/fa';
 import {Link} from "react-router-dom";
 
-const StockTable = ({stocks}) => {
+const StockTable = (
+    {stocks}
+) => {
 
 
     return (
@@ -16,28 +18,27 @@ const StockTable = ({stocks}) => {
 
             <section className='categoryList'>
                 <div className='table-list'>
-                    <table className="category-table">
-                        <tr className='category-table-title'>
-                            <th className='stock-th'>Código</th>
-                            <th className='stock-th'>Descripcion</th>
-                            <th className='stock-th'>Category</th>
-                            <th className='stock-th'>Cantidad</th>
-                            <th className='stock-th'>Modificar</th>
-                        </tr>
+                    <tr className='category-table-title'>
+                        <th className='stock-th'>Código</th>
+                        <th className='stock-th'>Descripcion</th>
+                        <th className='stock-th'>Category</th>
+                        <th className='stock-th'>Cantidad</th>
+                        <th className='stock-th'>Modificar</th>
+                    </tr>
 
-                        {stocks.map(stock =>
-                            <tr className='category-table-row' key={stock.id} stock={stock}>
-                                <td className="category-td">{stock.codigo}</td>
-                                <td className="category-td">{stock.descripcion}</td>
-                                <td className="category-td">{stock.category}</td>
-                                <td className="category-td" style={stock.cantidad === 0 ? {fontSize: 25, color: "red"} : {}}>{stock.cantidad}</td>
-                                <td className="category-td-link">
-                                    <Link className="btn-orange-link" to="/new"
-                                          state={{data: stock}}><FaEdit fixedWidth style={{color: 'red'}}/></Link>
-                                </td>
-                            </tr>
-                        )}
-                    </table>
+                    {stocks.map((stock =>
+                        <tr className='category-table-row' key={stock.id} stock={stock}>
+                            <td className="category-td">{stock.codigo}</td>
+                            <td className="category-td">{stock.descripcion}</td>
+                            <td className="category-td">{stock.category}</td>
+                            <td className="category-td" style={stock.cantidad === 0 ? {fontSize: 25, color: "red"} : {}}>{stock.cantidad}</td>
+                            <td className="category-td-link">
+                                <Link className="btn-orange-link" to="/new"
+                                      state={{data: stock}}><FaEdit fixedWidth style={{color: 'red'}}/></Link>
+                            </td>
+                        </tr>
+                        )
+                    )}
                 </div>
             </section>
         </div>
