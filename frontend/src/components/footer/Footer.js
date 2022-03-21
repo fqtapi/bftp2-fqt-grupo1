bcd2bb5004dd1152509812fbc4d663c34387e489
Laura Contreras <m.laura.cp89@gmail.com>
2022-03-21 10:24:05 +0100

    responsive footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -7,19 +7,19 @@ function Footer() {
     return (
         <div className="footer">
             <div className="thin">
-                <img src={logo} className="logo"/>
+                <img src={logo} className="footer-logo"/>
                 <h4> FUNDACION QUIERO TRABAJO</h4>
             </div>
             <div className="wide">
-                <p> Copyright &copy;2022 Fundación Quiero Trabajo | Todos los Derechos Reservados </p>
+                <p className="footer-p"> Copyright &copy;2022 Fundación Quiero Trabajo | Todos los Derechos Reservados </p>
             </div>
             <div className="thinner">
                 <ul>
                     <h3>OFICINA BARCELONA</h3>
-                    <li>Ronda Sant Pere 44, Entresuelo 2ª</li>
-                    <li>Metro: Urquinaona L1, L4</li>
-                    <li>info@quierotrabajo.org</li>
-                    <li>93 137 06 63</li>
+                    <li className="li-footer">Ronda Sant Pere 44, Entresuelo 2ª</li>
+                    <li className="li-footer">Metro: Urquinaona L1, L4</li>
+                    <li className="li-footer">info@quierotrabajo.org</li>
+                    <li className="li-footer">93 137 06 63</li>
                 </ul>
             </div>
 
